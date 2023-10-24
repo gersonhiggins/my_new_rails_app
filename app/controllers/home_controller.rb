9b9db121a469559a45ba4a75e714df488641516a
gersonhiggins <gahg.10gerson@gmail.com>
2023-10-24 15:30:47 -0400

    functionality

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def random_greeting
+    random_message = Message.order('RANDOM()').first
+    response.headers['Content-Type'] = 'application/json'
+    render json: { greeting: random_message.content }
+  end
+end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    // Make an HTTP request to fetch the random greeting
+    fetch("/random_greeting")
+      .then(response => response.json())
+      .then(data => setMessage(data.greeting))
+      .catch(error => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>{message}</p>
+    </div>
+  );
+
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root'),
+);
