07afda2402aa48b90d4a86348fe732127007feab
gersonhiggins <gahg.10gerson@gmail.com>
2023-10-24 20:22:05 -0400

    requested changes

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,12 +4,15 @@ import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './redux/store';
 import App from './components/greeting';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
   document.getElementById('root')
 );
